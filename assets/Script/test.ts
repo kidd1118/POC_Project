cc94f3e5223d7cba6c38fd0e2bc39b76570bc8d1
Leo.Lee <Leo.Lee@xuenn.com>
2018-05-02 17:37:13 +0800

    implement to animation

--- assets/Script/test.ts
+++ assets/Script/test.ts
@@ -6,24 +6,41 @@ export class test extends cc.Component {
     @property(cc.Node)
     private btnPlaySpine: cc.Node;
 
+    @property(cc.Node)
+    private btnPlaySpriteSheet: cc.Node;
+
     @property(sp.Skeleton)
     private spHit: sp.Skeleton;
 
+    @property(cc.Animation)
+    private animation: cc.Animation;
+
     @property(cc.AnimationClip)
-    private mc: cc.AnimationClip;
+    private animationClip: cc.AnimationClip;
+
+    @property(cc.SpriteAtlas)
+    private spriteAtlas: cc.SpriteAtlas;
+
+    @property(cc.SpriteFrame)
+    private spriteFrame: cc.SpriteFrame;
+
+    @property(cc.Sprite)
+    private sprite: cc.Sprite;
 
     protected onLoad() {
         this.spHit.paused = true;
+        this.animation.stop();
 
         let btnPlaySpine: cc.Node = cc.find("Canvas/btnPlaySpine");
         btnPlaySpine.on(cc.Node.EventType.MOUSE_DOWN, ()=> {
             this.spHit.paused = !this.spHit.paused;
         });
 
-        //this.btnPlaySpine.on(cc.Node.EventType.MOUSE_DOWN, ()=> {
-        //    this.spHit.paused = !this.spHit.paused;
-        //});
+        this.btnPlaySpriteSheet.on(cc.Node.EventType.MOUSE_DOWN, ()=> {
+            this.animation.play();
+        });
 
-        //let ButtonBase = this.btnPlaySpine.getComponent('ButtonBase');
+        this.spriteFrame.setTexture('asstes/Texture/Ch_attck.plist');
+        var aa = this.spriteFrame.getTexture();
 	}
 }
