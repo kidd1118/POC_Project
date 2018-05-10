3d814c629faf14c4eff117ecbdfbae417ab7a707
Willien.chen <07449.willien.chen@ict888.net>
2018-05-10 15:14:27 +0800

    Add Particle
    Add different particle to poc

--- assets/Script/PocParticle.ts
+++ assets/Script/PocParticle.ts
@@ -6,16 +6,35 @@ const { ccclass, property } = cc._decorator;
 export default class PocParticle extends PocCommon {
 
     start() {
-        this.curType = "particle_texture";
+        cc.loader.loadRes("particle_config", this._onLoadConfigComplete.bind(this));
+    }
+
+    //載入 config 完成
+    private _onLoadConfigComplete(error, result) {
+        this.config = result;
+        this.list = result["keys"];
+
+        this.curType = this.list[0];
         this.isReady = true;
     }
 
     _onAdd() {
         let node = this._generateNewNode();
+        node.setScale(0.5, 0.5);
+
         let component = node.addComponent(cc.ParticleSystem);
-        component.file = cc.url.raw("resources/particle/particle_texture.plist");
+        let data = this.config[this.curType];
+        component.file = cc.url.raw(data["path"]);
         component.resetSystem();
-        this.displayWindow.addChild(node);
+
+        let child: cc.Node = this.displayWindow.getChildByName(this.curType);
+        if (child == null){
+            child = new cc.Node();
+            child.name = this.curType;
+            this.displayWindow.addChild(child);
+        }
+
+        child.addChild(node);
         this.count++;
     }
 
