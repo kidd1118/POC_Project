112dcc5c41f953da0b7e7c3ae4b7d19ed75cb6eb
Willien.chen <07449.willien.chen@ict888.net>
2018-05-10 14:30:32 +0800

    add particle test to poc v2

--- assets/Script/PocSpriteSheet.ts
+++ assets/Script/PocSpriteSheet.ts
@@ -1,33 +1,10 @@
 import SpriteSheetAnimation from "./SpriteSheetAnimation";
+import PocCommon from "./PocCommon";
 
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class PocSpriteSheet extends cc.Component {
-
-    @property(cc.Label)
-    label_type: cc.Label = null;
-
-    @property(cc.Label)
-    label_count: cc.Label = null;
-
-    @property(cc.Node)
-    displayWindow: cc.Node = null;
-
-    @property(cc.Node)
-    btn_add: cc.Node = null;
-
-    @property(cc.Node)
-    btn_next: cc.Node = null;
-
-    @property(cc.Node)
-    btn_prev: cc.Node = null;
-
-    private curType = "";
-    private index: number = 0;
-    private list: string[] = [];
-    private config: object;
-    private count: number = 0;
+export default class PocSpriteSheet extends PocCommon {
 
     start() {
         cc.loader.loadRes("spritesheet_config", this._onLoadConfigComplete.bind(this));
@@ -37,39 +14,25 @@ export default class PocSpriteSheet extends cc.Component {
     private _onLoadConfigComplete(error, result) {
         this.config = result;
         this.list = result["keys"];
-        this.btn_add.on(cc.Node.EventType.TOUCH_END, this._onAdd.bind(this));
-        this.btn_next.on(cc.Node.EventType.TOUCH_END, this._onNext.bind(this));
-        this.btn_prev.on(cc.Node.EventType.TOUCH_END, this._onPrev.bind(this));
 
         this.curType = this.list[0];
-        this.label_type.string = this.curType;
+        this.isReady = true;
     }
 
-    private _onNext() {
-        if (++this.index >= this.list.length)
-            this.index = 0;
-        this.curType = this.list[this.index];
-        this.label_type.string = this.curType;
-    }
-
-    private _onPrev() {
-        if (--this.index < 0)
-            this.index = this.list.length - 1;
-        this.curType = this.list[this.index];
-
-        this.label_type.string = this.curType;
-    }
-
-    private _onAdd() {
-        let node = new cc.Node();
+    _onAdd() {
+        let node = this._generateNewNode();
         let ani = node.addComponent(SpriteSheetAnimation);
         let data = this.config[this.curType]
         ani.Apply(data["path"], data["clip"]);
-        let x = this.displayWindow.width / 2 - Math.random() * this.displayWindow.width;
-        let y = this.displayWindow.height / 2 - Math.random() * this.displayWindow.height;
-        node.setPosition(x, y);
-        this.displayWindow.addChild(node);
 
-        this.label_count.string = "Count : " + (this.count++)
+        let child: cc.Node = this.displayWindow.getChildByName(this.curType);
+        if (child == null){
+            child = new cc.Node();
+            child.name = this.curType;
+            this.displayWindow.addChild(child);
+        }
+
+        child.addChild(node);
+        this.count++;
     }
 }
